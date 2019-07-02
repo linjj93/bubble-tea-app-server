256d223c90c8c92f88d4ba9c6edeaeca652832cc
linjj93 <linjj93@hotmail.com>
2019-07-02 20:53:58 +0800

    Add authenticate middleware

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 require("./db");
+const authenticateUser = require("./middleware/auth");
 const express = require("express");
 const app = express();
 const drinksRouter = require("./routes/drinks.route");
+const usersRouter = require("./routes/users.route");
 app.use(express.json());
-app.use("/drinks", drinksRouter);
-app.get("/", (req, res, next) => {
-  res.status(200).json("Hello World!");
-});
+
+//public route
+app.use("/users", usersRouter);
+
+//private route
+app.use("/drinks", authenticateUser, drinksRouter);
 
 app.use((err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).json({ message: err.message });
   }
-  // console.log("error", err);
-  res.sendStatus(500).json({ message: err.message });
+  console.log("error", err);
+  res.status(500).json({ message: err.message });
 });
 
 module.exports = app;
