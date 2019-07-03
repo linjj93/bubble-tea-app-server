793de90b8da65591852b04f2bf8c763a6e55617d
linjj93 <linjj93@hotmail.com>
2019-07-03 16:29:18 +0800

    Add test for Delete and Put

--- tests/users.route.test.js
+++ tests/users.route.test.js
@@ -27,12 +27,12 @@ describe("app", () => {
 
   beforeEach(async () => {
     await db.dropDatabase();
+    const usersCollection = db.collection("users");
+    await usersCollection.insertMany(usersData);
   });
 
   describe("authentication", () => {
     it("POST /users/register should be able to register user if username has not been used before", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
       const newUser = {
         username: "newuser",
         password: "password"
@@ -42,20 +42,13 @@ describe("app", () => {
         .send(newUser)
         .set("Content-Type", "application/json");
 
-      const registeredUser = await usersCollection.findOne({
-        username: newUser.username
-      });
       expect(response.status).toEqual(200);
       expect(response.body.message).toEqual(
         `User ${newUser.username} created!`
       );
-      expect(registeredUser.username).toEqual(newUser.username);
     });
 
     it("POST /users/register should return error if username has been registered before", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
       const newUser = {
         username: "testuser2",
         password: "password"
@@ -71,8 +64,6 @@ describe("app", () => {
     });
 
     it("POST /users/register should return error if missing username", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
       const newUser = {
         password: "blahblah"
       };
@@ -86,8 +77,6 @@ describe("app", () => {
     });
 
     it("POST /users/register should return error if missing password", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
       const newUser = {
         username: "blahblah"
       };
@@ -101,8 +90,6 @@ describe("app", () => {
     });
 
     it("POST /users/register should return error if password is less than 8 characters", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
       const newUser = {
         username: "blahblah",
         password: "passwor"
@@ -119,9 +106,6 @@ describe("app", () => {
     });
 
     it("POST /users/login should allow login if user is registered", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
       const oldUser = {
         username: "testuser1",
         password: "password"
@@ -136,9 +120,6 @@ describe("app", () => {
     });
 
     it("POST /users/login should deny login if user is not registered", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
       const oldUser = {
         username: "testuser3",
         password: "password"
@@ -153,9 +134,6 @@ describe("app", () => {
     });
 
     it("POST /users/login should deny login if password is incorrect", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
       const oldUser = {
         username: "testuser1",
         password: "wrong_password"
@@ -170,11 +148,8 @@ describe("app", () => {
     });
   });
 
-  describe("get user data", () => {
+  describe.only("user interaction", () => {
     it("GET /users/testuser2 should show welcome message", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
       const login = await request(app)
         .post("/users/login")
         .send({ username: "testuser2", password: "abcdefgh" })
@@ -187,10 +162,8 @@ describe("app", () => {
         .set("Authorization", "Bearer " + login.body.token);
       expect(response.body.message).toEqual("Welcome, testuser2!");
     });
-    it("GET /users/testuser2/drinks should show testuser2 drinks", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
 
+    it("GET /users/testuser2/drinks should show testuser2 drinks", async () => {
       const login = await request(app)
         .post("/users/login")
         .send({ username: "testuser2", password: "abcdefgh" })
@@ -201,13 +174,11 @@ describe("app", () => {
       const response = await request(app)
         .get("/users/testuser2/drinks")
         .set("Authorization", "Bearer " + login.body.token);
-      expect(response.body).toMatchObject(usersData[1].drinks);
+
+      expect(response.body).toEqual(usersData[1].drinks);
     });
 
-    it("POST /users/testuser2/drinks should allow testuser2 to post a drink", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
+    it("POST /users/testuser2/drinks should add a drink and addition should persist", async () => {
       const login = await request(app)
         .post("/users/login")
         .send({ username: "testuser2", password: "abcdefgh" })
@@ -216,6 +187,7 @@ describe("app", () => {
       expect(login.body.username).toEqual("testuser2");
 
       const mockDrink = {
+        _id: "5d1a1d3eb555a81d301ce7a7",
         name: "Ovaltine",
         toppings: ["None"],
         price: 3.0,
@@ -230,13 +202,16 @@ describe("app", () => {
         .set("Content-Type", "application/json")
         .set("Authorization", "Bearer " + login.body.token);
 
+      expect(response.status).toEqual(201);
       expect(response.body).toMatchObject(mockDrink);
+
+      const userDrinksAfterAddition = await request(app)
+        .get("/users/testuser2/drinks")
+        .set("Authorization", "Bearer " + login.body.token);
+      expect(userDrinksAfterAddition.body).toHaveLength(3);
     });
 
     it("DELETE /users/testuser2/drinks/:id should allow testuser2 to delete a drink and removal should persist", async () => {
-      const usersCollection = db.collection("users");
-      await usersCollection.insertMany(usersData);
-
       const login = await request(app)
         .post("/users/login")
         .send({ username: "testuser2", password: "abcdefgh" })
@@ -244,25 +219,49 @@ describe("app", () => {
       expect(login.status).toEqual(200);
       expect(login.body.username).toEqual("testuser2");
 
-      const userData = await request(app)
+      const userDrinks = await request(app)
         .get("/users/testuser2/drinks")
         .set("Authorization", "Bearer " + login.body.token);
 
-      const drinkToDelete = userData.body[0];
-
-      expect(userData.body).toHaveLength(2);
+      expect(userDrinks.body).toHaveLength(2);
+      const drinkToDelete = userDrinks.body[0];
 
       const response = await request(app)
         .delete(`/users/testuser2/drinks/${drinkToDelete._id}`)
         .set("Authorization", "Bearer " + login.body.token);
 
       expect(response.body).toMatchObject(drinkToDelete);
+      const userDrinksAfterDeletion = await request(app)
+        .get("/users/testuser2/drinks")
+        .set("Authorization", "Bearer " + login.body.token);
+      expect(userDrinksAfterDeletion.body).toHaveLength(1);
+    });
 
-      const userDataAfterDeletion = await request(app)
+    it("PUT /users/testuser2/drinks/id should update drink", async () => {
+      const login = await request(app)
+        .post("/users/login")
+        .send({ username: "testuser2", password: "abcdefgh" })
+        .set("Content-Type", "application/json");
+      expect(login.status).toEqual(200);
+      expect(login.body.username).toEqual("testuser2");
+
+      const userDrinks = await request(app)
         .get("/users/testuser2/drinks")
         .set("Authorization", "Bearer " + login.body.token);
 
-      expect(userDataAfterDeletion.body).toHaveLength(1);
+      const drinkToUpdate = userDrinks.body[1];
+
+      const fieldsToUpdate = {
+        price: 5.0
+      };
+      const update = await request(app)
+        .put(`/users/testuser2/drinks/${drinkToUpdate._id}`)
+        .send(fieldsToUpdate)
+        .set("Content-Type", "application/json")
+        .set("Authorization", "Bearer " + login.body.token);
+
+      expect(update.body.price).toEqual(fieldsToUpdate.price);
+      expect(update.status).toEqual(200);
     });
   });
 });
