7a4914b3782f4220ce768452c010eadbb7104a1a
linjj93 <linjj93@hotmail.com>
2019-07-08 23:35:15 +0800

    Remove comments

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ const usersRouter = require("./routes/users.route");
 app.use(cors());
 app.use(express.json());
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
 app.use("/users", usersRouter);
 
 app.use((err, req, res, next) => {
