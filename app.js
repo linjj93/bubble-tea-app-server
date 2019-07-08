64efe709a74694f5c008de66d2ca1fdd516aadbe
linjj93 <linjj93@hotmail.com>
2019-07-08 23:32:33 +0800

    use cors

--- app.js
+++ app.js
@@ -4,27 +4,18 @@ const express = require("express");
 const app = express();
 const usersRouter = require("./routes/users.route");
 
-// app.options("*", cors());
-
-// app.use(
-//   cors({
-//     origin: "*",
-//     credentials: true
-//   })
-// );
-
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 app.use("/users", usersRouter);
 
 app.use((err, req, res, next) => {
